24af13433f60f772891206e51da85b4564f0bb54
TongThanhVinh <thanhvinh281104@gmail.com>
2025-09-15 09:30:01 +0700

    nop bai

---
+++ Chuong2_Cau6.py
@@ -0,0 +1,9 @@
+"""
+/ dùng để chia lấy phần thực
+// dùng để chia lấy phần nguyên
+% dùng để chia lấy dư
+** dùng để bình phương
+and được gọi là toán tử logic and (và). Nếu cả 2 toán tử đều có giá trị khác 0 thì điều kiện trở lên true
+or được gọi là toán tử logic or (hoặc). Nếu 1 trong 2 toán tử khác 0, thì điều kiện là true
+is dùng để kiểm tra 2 biến có cùng trỏ tới 1 vị trí trong bộ nhớ hay không
+"""
